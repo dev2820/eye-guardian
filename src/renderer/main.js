9acc1c6eeb2a418291323eca84f2101921913ab3
dev2820 <second28200@gmail.com>
2021-08-03 18:47:42 +0900

    fontawesome 적용

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,8 +5,14 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faUserSecret);
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
+Vue.component('font-awesome-icon',FontAwesomeIcon)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
