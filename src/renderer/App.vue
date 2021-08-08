f1e2db24846290c6dc1928cbaa2d9bf909f01520
dev2820 <second28200@gmail.com>
2021-08-08 15:20:18 +0900

    나눔 고딕 폰트 적용

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -16,5 +16,10 @@
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  font-family: 'Nanum_Gothic', 'Times New Roman', Times, serif;
+}
+@font-face { 
+  font-family: 'Nanum_Gothic';
+  src: url('./assets/fonts/Nanum_Gothic/NanumGothic-Regular.ttf');
 }
 </style>
