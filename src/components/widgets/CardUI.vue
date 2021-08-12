a07ccd1eeb2477e72693820ca52f5db2816c29fe
dev2820 <second28200@gmail.com>
2021-08-12 22:46:21 +0900

    cardui 생성

---
+++ src/components/widgets/CardUI.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="card">
+    <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.card {
+    min-width:120px;
+    max-width:200px;
+    margin:0 20px 20px 0;
+    border-radius:4px;
+    padding:5px;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+}
+</style>
