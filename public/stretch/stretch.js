cbd5772bd507f56bd1528ffa4d6182aa0a70a5c4
dev2820 <second28200@gmail.com>
2021-08-18 17:46:57 +0900

    영상 정지시 종료

--- public/stretch/stretch.js
+++ public/stretch/stretch.js
@@ -5,19 +5,13 @@ const videoEl = document.getElementById('stretchVideo');
 const exitButton = document.getElementById('exitButton');
 exitButton.addEventListener('click',()=>{
     exitStretch();
-},false);
-videoEl.addEventListener('play',()=>{
-    // isplay=true;
-})
-videoEl.addEventListener('paused',()=>{//플레이가 끝나면
-    // isended=true;
     closeStretchGuide();
-});
+},false);
 videoEl.addEventListener('ended',()=>{//플레이가 끝나면
-    // isended=true;
     closeStretchGuide();
 });
 ipcRenderer.on('PLAY_STRETCH_GUIDE',()=>{//플레이 시작 message
+    videoEl.currentTime = 0;
     videoEl.play();
 })
 function closeStretchGuide(){
