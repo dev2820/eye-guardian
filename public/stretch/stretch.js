671b94563cdf3883722947bdc9f4b93d516b5c56
dev2820 <second28200@gmail.com>
2021-08-18 17:33:15 +0900

    스트레칭 끄기 버튼 생성

--- public/stretch/stretch.js
+++ public/stretch/stretch.js
@@ -2,9 +2,17 @@ const { ipcRenderer } = require("electron");
 let isplay=false;
 let isended=false;
 const videoEl = document.getElementById('stretchVideo');
+const exitButton = document.getElementById('exitButton');
+exitButton.addEventListener('click',()=>{
+    exitStretch();
+},false);
 videoEl.addEventListener('play',()=>{
     // isplay=true;
 })
+videoEl.addEventListener('paused',()=>{//플레이가 끝나면
+    // isended=true;
+    closeStretchGuide();
+});
 videoEl.addEventListener('ended',()=>{//플레이가 끝나면
     // isended=true;
     closeStretchGuide();
@@ -14,4 +22,7 @@ ipcRenderer.on('PLAY_STRETCH_GUIDE',()=>{//플레이 시작 message
 })
 function closeStretchGuide(){
     ipcRenderer.send('HIDE_STRETCH_GUIDE',true);
+}
+function exitStretch() {
+    videoEl.pause();
 }
